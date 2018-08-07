b62eaf469928e4be661cba26380e08506186540b
Brian <juniortoad1738@gmail.com>
2018-08-06 23:14:25 -0400

    mongoose, passport, EJS engine

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
+require('dotenv').config()
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
 var bodyParser = require('body-parser');
+var LocalStrategy = require('passport-local').Strategy;
+var back = require('express-back');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var braintree = require('braintree');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+
+var engines = require('consolidate');
+
+app.engine('html', engines.ejs);
 app.set('view engine', 'jade');
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -22,8 +33,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// PASSPORT STUFF
+app.use(require('cookie-session')({
+  name: 'session',
+  secret: process.env.SESSION_SECRET || 'secret',
+  resave: false,
+  saveUninitialized: false
+}));
+// app.use back has to go after express-session STUFF
+app.use(back());
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', routes);
 app.use('/users', users);
+
+// add authenticate method
+var User = require('./models/user');
+passport.use( new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -46,6 +75,7 @@ if (app.get('env') === 'development') {
   });
 }
 
+mongoose.connect('mongodb://localhost/authentication-app', { useMongoClient: true });
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
